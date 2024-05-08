15c51eec03d906f67653da800ed121492bff69a1
Zsystem <jmeneguzzi@gmail.com>
2024-05-08 15:53:00 -0300

    v3

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,4 @@ export const environment = {
     URL_BACKEND: 'http://192.168.1.101:8000',
     URL_SERVICIOS: 'http://192.168.1.101:8000/api',
     URL_FRONEND: 'http://192.168.1.99:4200'
-
-
-
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    URL_BACKEND: 'http://api.zsystem.com.ar',
+    URL_SERVICIOS: 'http://api.zsystem.com.ar/api',
+    URL_FRONEND: 'http://admin.zsystem.com.ar'
+};
